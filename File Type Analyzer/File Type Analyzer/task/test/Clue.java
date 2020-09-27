459752d866fe25b00795d68b08f6a5102af2a760
an-tran <an.tran@big-labs.com>
2020-09-27 17:25:56 +0700

    some additional file

--- File Type Analyzer/File Type Analyzer/task/test/Clue.java
+++ File Type Analyzer/File Type Analyzer/task/test/Clue.java
@@ -1,12 +1,7 @@
-
 public class Clue {
-    public String first;
-    public String second;
-    public String feedback;
+    public String response;
 
-    public Clue(String first, String second, String feedback) {
-        this.first = first;
-        this.second = second;
-        this.feedback = feedback;
+    public Clue(String response) {
+        this.response = response;
     }
 }
